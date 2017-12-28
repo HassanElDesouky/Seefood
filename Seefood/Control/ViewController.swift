8836899f41e0c7f34d5f7a605ae84c0f6fbc6acf
hassaneldesouky <hassaneldesouky@icloud.com>
2017-12-28 05:14:47 +0200

    Complete version 1

---
+++ Seefood/Control/ViewController.swift
@@ -0,0 +1,94 @@
+//
+//  ViewController.swift
+//  Seefood
+//
+//  Created by Hassan El Desouky on 12/28/17.
+//  Copyright © 2017 Hassan El Desouky. All rights reserved.
+//
+
+//MARK: Libraries
+import UIKit
+import CoreML
+import Vision
+
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    //MARK: Outlets
+    @IBOutlet weak var theImageView: UIImageView!
+    
+    
+    //MARK: Declare vars and cons
+    let imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Implementing the camera into the app
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
+    }
+    
+    
+    //MARK: Methods
+    
+    // A delegate method (image || video)
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+     
+        if let userPickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+                theImageView.image = userPickedImage
+            
+            guard let ciimage = CIImage(image: userPickedImage) else {
+                fatalError("Could not convert the image to CIImage!")
+            }
+            
+            detect(image: ciimage)
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    
+    
+    func detect(image: CIImage) {
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Loading core ML failed.")
+        }
+        
+        // Vision CoreML request
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Model failed to process image.")
+            }
+            
+            if let firstResult =  results.first {
+                if firstResult.identifier.contains("hotdog") {
+                    self.navigationItem.title = "Hotdog!"
+                } else {
+                    self.navigationItem.title = "Not Hotdog!"
+                }
+            }
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+        try handler.perform([request])
+        }
+        catch {
+            print(error)
+        }
+        
+    }
+    
+    
+    
+    //MARK: Actions
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        present(imagePicker, animated: true, completion: nil)
+    }
+}
+
